839f522a43b0cd25a7bbf0db22f4f5ae2b037b2d
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-02-28 20:53:16 -0800

    chore: Make tapes better
    Now tapes are based on the test calling the API rather than the API being used.
    
    Also, added a test for getting the PrivateUser when getting authenticated user.

--- pulpogato-rest-fpt/src/test/java/io/github/pulpogato/rest/api/UsersApiIntegrationTest.java
+++ pulpogato-rest-fpt/src/test/java/io/github/pulpogato/rest/api/UsersApiIntegrationTest.java
@@ -3,6 +3,7 @@ package io.github.pulpogato.rest.api;
 import io.github.pulpogato.test.ProxyApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import reactor.netty.http.client.HttpClient;
+
+import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,12 +34,16 @@ class UsersApiIntegrationTest {
     HttpServiceProxyFactory factory;
 
     @BeforeEach
-    void setUp() {
+    void setUp(TestInfo testInfo) {
         HttpClient httpClient = HttpClient.create();
+
+        String classPart = testInfo.getTestClass().map(Class::getName).map(name -> name.replace(".", "/")).orElseThrow();
+        String methodPart = testInfo.getTestMethod().map(Method::getName).orElseThrow();
 
         final var webClient = WebClient.builder()
                 .baseUrl("http://localhost:" + randomServerPort)
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultHeader("TapeName", classPart + "/" + methodPart)
                 .build();
 
         factory = HttpServiceProxyFactory.builder()
@@ -45,7 +52,7 @@ class UsersApiIntegrationTest {
     }
 
     @Test
-    void testGetAuthenticated() {
+    void testGetAuthenticatedPublic() {
         UsersApi api = factory.createClient(UsersApi.class);
         ResponseEntity<UsersApi.GetAuthenticated200> authenticated = api.getAuthenticated();
         assertThat(authenticated.getStatusCode().is2xxSuccessful()).isTrue();
@@ -58,6 +65,24 @@ class UsersApiIntegrationTest {
         var privateUser = body.getPrivateUser();
         assertThat(privateUser.getId()).isNotNull();
         assertThat(privateUser.getLogin()).isNotNull();
+    }
+
+    @Test
+    void testGetAuthenticatedPrivate() {
+        UsersApi api = factory.createClient(UsersApi.class);
+        ResponseEntity<UsersApi.GetAuthenticated200> authenticated = api.getAuthenticated();
+        assertThat(authenticated.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(authenticated.getBody())
+                .isNotNull()
+                .isInstanceOf(UsersApi.GetAuthenticated200.class);
+        var body = authenticated.getBody();
+
+        assertThat(body.getPrivateUser()).isNotNull();
+        var privateUser = body.getPrivateUser();
+        assertThat(privateUser.getId()).isNotNull();
+        assertThat(privateUser.getLogin()).isNotNull();
+        assertThat(privateUser.getPlan()).isNotNull();
+        assertThat(privateUser.getPlan().getName()).isNotNull();
     }
 
 }
