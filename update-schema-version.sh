a45c64a9dfc6255cae6ea6d1ecc69068a52d5121
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-03-14 23:48:07 -0700

    chore: Clean up after update schema version

--- update-schema-version.sh
+++ update-schema-version.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if git diff --quiet; then
+    echo "No changes to main. Proceeding..."
+else
+    echo "Changes in main, please commit or stash them"
+    exit 1
+fi
+
 git checkout main
 git pull --rebase
-git checkout -b update-schema-version
 bun update
 
 if git diff --quiet; then
     echo "No changes to rest-api-description"
+    exit 2
 else
+    git checkout -b update-schema-version
     git add .
-    export RAD_VERSION=$(bun pm ls | grep rest-api-description | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | cut -d '#' -f 2)
+    RAD_VERSION=$(bun pm ls | grep rest-api-description | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | cut -d '#' -f 2)
+    export RAD_VERSION
     git commit -m "chore(deps): Update rest-api-description to $RAD_VERSION"
     git push origin update-schema-version
     gh pr create \
@@ -18,4 +27,6 @@ else
         --base main \
         --head update-schema-version
     gh pr merge --auto --merge
+    git checkout main
+    git branch -D update-schema-version
 fi
