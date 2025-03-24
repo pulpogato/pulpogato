6aebe007e19f55c5bf9b874cf207b989303c91ad
Rahul Somasunderam <rahul.som@gmail.com>
2025-03-24 13:07:56 -0700

    chore(deps): Update JUnit to 5.11.4
    Also, enable develocity for plugin.

--- gradle/pulpogato-rest-codegen/settings.gradle.kts
+++ gradle/pulpogato-rest-codegen/settings.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    id("com.gradle.develocity").version("3.19.2")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/terms-of-service")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 rootProject.name = "pulpogato-rest-codegen"
 
 dependencyResolutionManagement {
