78e83f917c3d7cf2e9f7e403c8503bf554b2aa96
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-03-25 21:35:19 -0700

    chore: Add pitest to plugin

--- gradle/pulpogato-rest-codegen/build.gradle.kts
+++ gradle/pulpogato-rest-codegen/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     `kotlin-dsl`
     alias(libs.plugins.ktlint)
     alias(libs.plugins.testLogger)
+    alias(libs.plugins.pitest)
 }
 
 repositories {
@@ -53,4 +54,14 @@ testlogger {
     showPassed = false
     showSkipped = true
     showFailed = true
+}
+
+pitest {
+    timestampedReports = false
+    junit5PluginVersion.set(libs.versions.pitestJunit5Plugin) // Look here for latest version - https://github.com/pitest/pitest-junit5-plugin/tags
+    pitestVersion.set(libs.versions.pitest) // Look here for latest version - https://github.com/hcoles/pitest/releases
+    mutators.set(setOf("ALL"))
+    outputFormats.set(setOf("XML", "HTML"))
+    targetClasses.set(listOf("io.github.pulpogato.restcodegen.*"))
+    excludedTestClasses.set(listOf("io.github.pulpogato.restcodegen.RestCodegenPluginTest"))
 }
