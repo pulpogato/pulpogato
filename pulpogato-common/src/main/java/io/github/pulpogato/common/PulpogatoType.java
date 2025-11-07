396d2dc0b718e4d3d518b226bca309a0ae829c14
Rahul Somasunderam <rahul.som@gmail.com>
2025-11-07 15:28:48 -0800

    docs: Add javadoc to PulpogatoType

--- pulpogato-common/src/main/java/io/github/pulpogato/common/PulpogatoType.java
+++ pulpogato-common/src/main/java/io/github/pulpogato/common/PulpogatoType.java
@@ -1,5 +1,13 @@
 package io.github.pulpogato.common;
 
+/**
+ * An interface representing a type that is defined by Pulpogato.
+ */
 public interface PulpogatoType {
+    /**
+     * Converts the object to its code representation.
+     *
+     * @return A string representing the java code to create the object.
+     */
     String toCode();
 }
