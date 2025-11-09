ed279098fa79d564bf503529b20cf58a4bf852d5
Rahul Somasunderam <rahul.som@gmail.com>
2025-11-09 11:05:01 -0800

    chore: Remove Nested in StringOrIntegerTest
    It has only one Nested class and can be flattened.

--- pulpogato-common/src/test/java/io/github/pulpogato/common/StringOrIntegerTest.java
+++ pulpogato-common/src/test/java/io/github/pulpogato/common/StringOrIntegerTest.java
@@ -2,33 +2,29 @@ package io.github.pulpogato.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class StringOrIntegerTest {
-    @Nested
-    class ToCodeTests {
-        @Test
-        void testToCodeWithInteger() {
-            StringOrInteger value = StringOrInteger.builder().integerValue(3L).build();
-            assertThat(value.toCode())
-                    .isEqualTo(
-                            """
-                    io.github.pulpogato.common.StringOrInteger.builder()
-                        .integerValue(3L)
-                        .build()""");
-        }
 
-        @Test
-        void testToCodeWithString() {
-            StringOrInteger value =
-                    StringOrInteger.builder().stringValue("example").build();
-            assertThat(value.toCode())
-                    .isEqualTo(
-                            """
-                    io.github.pulpogato.common.StringOrInteger.builder()
-                        .stringValue("example")
-                        .build()""");
-        }
+    @Test
+    void testToCodeWithInteger() {
+        StringOrInteger value = StringOrInteger.builder().integerValue(3L).build();
+        assertThat(value.toCode())
+                .isEqualTo(
+                        """
+                        io.github.pulpogato.common.StringOrInteger.builder()
+                            .integerValue(3L)
+                            .build()""");
+    }
+
+    @Test
+    void testToCodeWithString() {
+        StringOrInteger value = StringOrInteger.builder().stringValue("example").build();
+        assertThat(value.toCode())
+                .isEqualTo(
+                        """
+                        io.github.pulpogato.common.StringOrInteger.builder()
+                            .stringValue("example")
+                            .build()""");
     }
 }
