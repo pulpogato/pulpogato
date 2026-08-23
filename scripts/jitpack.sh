f4de4c74d6ad8deab4ad6255af5a9a3f8f9ecc12
Rahul Somasunderam <rahul.som@gmail.com>
2026-08-23 12:32:33 -0700

    fix: Quote group and version in jitpack script
    Also document the script.

--- scripts/jitpack.sh
+++ scripts/jitpack.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# This is invoked by Jitpack based on the jitpack.yml configuration file.
+# It is used to build the project and publish it to the local Maven repository.
+# Jitpack then publishes the artifacts to its own Maven repository.
+
 set -eux
 
 ./gradlew clean \
-    -Pgroup=$GROUP \
-    -Pversion=$VERSION \
+    -Pgroup="$GROUP" \
+    -Pversion="$VERSION" \
     --console=plain \
     --max-workers=4 \
     -xtest assemble publishToMavenLocal
